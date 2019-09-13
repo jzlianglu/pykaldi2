aa1009ab31a53a80a476ad883f4876df935d6de0
liang.lu <lial@microsoft.com>
2019-09-13 18:36:47 +0000

    update chain training script

--- train_chain.py
+++ train_chain.py
@@ -39,7 +39,6 @@ import horovod.torch as hvd
 import kaldi.fstext as kaldi_fst
 import kaldi.hmm as kaldi_hmm
 import kaldi.matrix as kaldi_matrix
-import kaldi.cudamatrix as kaldi_cudamatrix
 import kaldi.lat as kaldi_lat
 import kaldi.decoder as kaldi_decoder
 import kaldi.alignment as kaldi_align
@@ -59,23 +58,22 @@ def main():
     parser.add_argument("-config")       
     parser.add_argument("-data", help="data yaml file")
     parser.add_argument("-dataPath", default='', type=str, help="path of data files") 
-    parser.add_argument("-seed_model", help="the seed nerual network model")                                                                                  
+    parser.add_argument("-seed_model", default='', help="the seed nerual network model")                                                                                  
     parser.add_argument("-exp_dir", help="the directory to save the outputs") 
     parser.add_argument("-transform", help="feature transformation matrix or mvn statistics")
-    parser.add_argument("-criterion", type=str, choices=["mmi", "mpfe", "smbr"], help="set the sequence training crtierion") 
-    parser.add_argument("-trans_model", help="the HMM transistion model directory") 
-    parser.add_argument("-prior_path", help="the prior for decoder, usually named as final.occs in kaldi setup")
-    parser.add_argument("-den_dir", help="the decoding graph directory to find HCLG and words.txt files")
-    parser.add_argument("-lang_dir", help="the lexicon directory to find L.fst")
+    parser.add_argument("-ali_dir", help="the directory to load trans_model and tree used for alignments") 
+    parser.add_argument("-lang_dir", help="the lexicon directory to load L.fst")
+    parser.add_argument("-chain_dir", help="the directory to load trans_model, tree and den.fst for chain model")
     parser.add_argument("-lr", type=float, help="set the learning rate")
-    parser.add_argument("-ce_ratio", default=0.1, type=float, help="the ratio for ce regularization") 
     parser.add_argument("-momentum", default=0, type=float, help="set the momentum") 
     parser.add_argument("-weight_decay", default=1e-4, type=float, help="set the L2 regularization weight") 
     parser.add_argument("-batch_size", default=32, type=int, help="Override the batch size in the config")                         
     parser.add_argument("-data_loader_threads", default=0, type=int, help="number of workers for data loading")
     parser.add_argument("-max_grad_norm", default=5, type=float, help="max_grad_norm for gradient clipping")                     
     parser.add_argument("-sweep_size", default=100, type=float, help="process n hours of data per sweep (default:100)")
-    parser.add_argument("-num_epochs", default=1, type=int, help="number of training epochs (default:1)") 
+    parser.add_argument("-num_epochs", default=1, type=int, help="number of training epochs (default:1)")
+    parser.add_argument("-anneal_lr_epoch", default=2, type=int, help="start to anneal the learning rate from this epoch")  
+    parser.add_argument("-aneal_lr_ratio", default=0.5, type=float, help="the ratio to anneal the learning rate ratio") 
     parser.add_argument('-print_freq', default=10, type=int, metavar='N', help='print frequency (default: 10)')
     parser.add_argument('-save_freq', default=1000, type=int, metavar='N', help='save model frequency (default: 1000)')
 
@@ -132,18 +130,15 @@ def main():
 
     model.cuda()
 
-    chain_net = NnetAM(lstm, model_config["hidden_size"]*2, 6048)
-    chain_net.cuda()
-
     # setup the optimizer
     optimizer = th.optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
 
     # Broadcast parameters and opterimizer state from rank 0 to all other processes.
-    hvd.broadcast_parameters(chain_net.state_dict(), root_rank=0)
+    hvd.broadcast_parameters(model.state_dict(), root_rank=0)
     hvd.broadcast_optimizer_state(optimizer, root_rank=0)
 
     # Add Horovod Distributed Optimizer
-    optimizer = hvd.DistributedOptimizer(optimizer, named_parameters=chain_net.named_parameters())
+    optimizer = hvd.DistributedOptimizer(optimizer, named_parameters=model.named_parameters())
 
     if os.path.isfile(args.seed_model):
         checkpoint = th.load(args.seed_model)                                            
@@ -159,36 +154,15 @@ def main():
         else:                                                
             model.load_state_dict(state_dict)                
         print("=> loaded checkpoint '{}' ".format(args.seed_model))                      
-    else:
-        sys.stderr.write('ERROR: The model file %s does not exist!\n'%(model_file))
-        sys.exit(0)      
 
-    HCLG = args.den_dir + "/HCLG.fst"
-    words_txt = args.den_dir + "/words.txt"
-    silence_phones = args.den_dir + "/phones/silence.csl"
-    ali_model = args.trans_model + "/final.mdl"
-    ali_tree = args.trans_model + "/tree"
+    ali_model = args.ali_dir + "/final.mdl"
+    ali_tree = args.ali_dir + "/tree"
     L_fst = args.lang_dir + "/L.fst"
     disambig = args.lang_dir + "/phones/disambig.int"
 
-    den_fst = kaldi_fst.StdVectorFst.read("/datadisk2/lial/librispeech/s5/exp/chain_cleaned/tdnn_1d_sp/den.fst")
-    chain_model_path = "/datadisk2/lial/librispeech/s5/exp/chain_cleaned/tdnn_1d_sp/0.trans_mdl"
-    chain_tree_path = "/datadisk2/lial/librispeech/s5/exp/chain_cleaned/tdnn_1d_sp/tree"
-
-    if not os.path.isfile(HCLG):
-        sys.stderr.write('ERROR: The HCLG file %s does not exist!\n'%(HCLG))
-        sys.exit(0)
-    
-    if not os.path.isfile(words_txt):
-        sys.stderr.write('ERROR: The words.txt file %s does not exist!\n'%(words_txt))
-        sys.exit(0)
- 
-    if not os.path.isfile(silence_phones):
-        sys.stderr.write('ERROR: The silence phone file %s does not exist!\n'%(silence_phones))
-        sys.exit(0)
-    with open(silence_phones) as f:
-        silence_ids = [int(i) for i in f.readline().strip().split(':')]
-        f.close()
+    den_fst = kaldi_fst.StdVectorFst.read(args.chain_dir + "/den.fst")
+    chain_model_path = args.chain_dir + "/0.trans_mdl"
+    chain_tree_path = args.chain_dir + "/tree"
 
     if os.path.isfile(chain_model_path):
        chain_trans_model = kaldi_hmm.TransitionModel()
@@ -205,7 +179,7 @@ def main():
      # chain supervision options
     supervision_opts = kaldi_chain.SupervisionOptions()
     supervision_opts.convert_to_pdfs = True
-    supervision_opts.frame_subsampling_factor = 1
+    supervision_opts.frame_subsampling_factor = 3
     supervision_opts.left_tolerance = 5
     supervision_opts.right_tolerance = 5
 
@@ -221,26 +195,24 @@ def main():
                                  transition_scale=1.0, 
                                  self_loop_scale=0.1, 
                                  acoustic_scale=0.1)
-    # compute the log prior
-    prior = kaldi_util.io.read_matrix(args.prior_path).numpy()
-    log_prior = th.tensor(np.log(prior[0]/np.sum(prior[0])), dtype=th.float)
+    den_graph = kaldi_chain.DenominatorGraph(den_fst, model_config["label_size"])
 
-    den = kaldi_chain.DenominatorGraph(den_fst, 6048)
-
-    chain_net.train()
+    model.train()
     for epoch in range(args.num_epochs): 
 
+        # anneal learning rate
+        if epoch > args.anneal_lr_epoch:
+            for param_group in optimizer.param_groups:
+                param_group['lr'] *= args.anneal_lr_ratio
+
         run_train_epoch(model, optimizer,
-                       log_prior.cuda(), 
                        train_dataloader, 
                        epoch, 
                        chain_trans_model,
                        chain_tree,
                        supervision_opts,
-                       silence_ids,
-                       chain_net,
                        aligner,
-                       den,
+                       den_graph,
                        chain_opts,
                        args)
 
@@ -250,22 +222,17 @@ def main():
             checkpoint['model']=model.state_dict()
             checkpoint['optimizer']=optimizer.state_dict()
             checkpoint['epoch']=epoch
-            output_file=args.exp_dir + '/model.se.'+ str(epoch) +'.tar'
+            output_file=args.exp_dir + '/chain.model.'+ str(epoch) +'.tar'
             th.save(checkpoint, output_file)
 
-def run_train_epoch(model, optimizer, log_prior, dataloader, epoch, trans_model, tree, supervision_opts, silence_ids, net, aligner, den, chain_opts, args):
+def run_train_epoch(model, optimizer, dataloader, epoch, trans_model, tree, supervision_opts, aligner, den, chain_opts, args):
     batch_time = utils.AverageMeter('Time', ':6.3f')
     losses = utils.AverageMeter('Loss', ':.4e')
     grad_norm = utils.AverageMeter('grad_norm', ':.4e')
     progress = utils.ProgressMeter(len(dataloader), batch_time, losses, grad_norm, 
                              prefix="Epoch: [{}]".format(epoch))
 
-    ce_criterion = nn.CrossEntropyLoss(ignore_index=-100, reduction='sum')
-    if args.criterion == "mmi":
-        criterion = ops.MMIFunction.apply
-    else:
-        criterion = ops.sMBRFunction.apply
-
+    criterion = ops.ChainObjtiveFunction.apply
     end = time.time()
     for i, batch in enumerate(dataloader):
         feat = batch["x"] 
@@ -273,26 +240,15 @@ def run_train_epoch(model, optimizer, log_prior, dataloader, epoch, trans_model,
         num_frs = batch["num_frs"]                       
         utt_ids = batch["utt_ids"]                       
         aux = batch["aux"]  #word labels for se loss
-                                       
-        x = feat.to(th.float32)                         
-        y = label.long()
-        x = x.cuda()    
-        y = y.cuda()                                
+    
+        x = feat.to(th.float32)
+        x = x.unfold(1, 1, 3).squeeze(-1)
+        x = x.cuda()   
+        y = label.squeeze(2) 
                                                 
-        prediction_ali = model(x)
-        prediction = net(x)
-        #ce_loss = ce_criterion(prediction.view(-1, prediction.shape[2]), y.view(-1))
-        #loss = args.ce_ratio * ce_loss
- 
+        prediction = model(x)
         for j in range(len(num_frs)):                   
-            loglike = prediction_ali[j,:,:]       
-            loglike_j = loglike[:num_frs[j],:]        
-            loglike_j = loglike_j - log_prior
-            text = th.from_numpy(aux[j][0][0].astype(int)).tolist()
-            
-            align_in = kaldi_matrix.Matrix(loglike_j.detach().cpu().numpy())
-            align_out = aligner.align(align_in, text) 
-            trans_ids = align_out["alignment"]
+            trans_ids = y[j, :num_frs[j]].tolist()
             phone_ali = aligner.to_phone_alignment(trans_ids)
             
             phones = list()
@@ -305,26 +261,9 @@ def run_train_epoch(model, optimizer, log_prior, dataloader, epoch, trans_model,
             supervision = kaldi_chain.proto_supervision_to_supervision(tree, trans_model, proto_supervision, True)
 
             loglike = prediction[j,:,:]
-            loglike_j = loglike[:num_frs[j],:]
-            loglike_j = kaldi_matrix.Matrix(loglike_j.detach().cpu().numpy())
-
-            nnet_out = kaldi_cudamatrix.CuMatrix().from_matrix(loglike_j)
-            grad_out = kaldi_cudamatrix.CuMatrix().from_size(nnet_out.num_rows(), nnet_out.num_cols())
-            print(supervision.num_sequences * supervision.frames_per_sequence)
-            print(nnet_out.num_rows())
-            print(supervision.label_dim, nnet_out.num_cols())
-
-            loss = kaldi_chain.compute_chain_objf_and_deriv(chain_opts, den, supervision, nnet_out, grad_out, None) 
+            loglike_j = loglike[:supervision.frames_per_sequence,:]
+            loss = criterion(loglike_j, den, supervision, chain_opts) 
             
-            grad_mat = kaldi_matrix.Matrix(nnet_out.num_rows(), nnet_out.num_cols())
-            grad_out.copy_to_mat(grad_mat)
-            print(loss)
-            print(grad_mat.numpy())
-
-                #loss += se_loss.cuda()
-           # except:
-           #     print("Warning: failed to align utterance {}, skip the utterance for SE loss".format(utt_ids[j]))        
-
         optimizer.zero_grad()
         loss.backward()
 
